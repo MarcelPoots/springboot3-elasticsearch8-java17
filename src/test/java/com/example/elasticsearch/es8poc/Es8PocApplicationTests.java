e7022e22e443e34aa19a15be38715d5132c42049
mpoots <mpoots@localhost>
2023-01-18 15:14:19 +0100

    Removed tests

--- src/test/java/com/example/elasticsearch/es8poc/Es8PocApplicationTests.java
+++ src/test/java/com/example/elasticsearch/es8poc/Es8PocApplicationTests.java
@@ -3,10 +3,10 @@ package com.example.elasticsearch.es8poc;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class Es8PocApplicationTests {
 
-	@Test
+	//@Test
 	void contextLoads() {
 	}
 
